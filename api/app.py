a8280157eeb1de88326ddd65caf99fed6361bb61
Hanish0 <128824988+Hanish0@users.noreply.github.com>
2025-03-26 17:26:48 +0530

    Added mongodb connection and test route

--- api/app.py
+++ api/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from api.models import task_collection
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return {'message': 'Task Manager API is running!'}
+    return {"message": "Welcome to Task Manager API!"}
+
+@app.route('/test-db')
+def test_db():
+    test_task = {"title": "Test task", "status": "pending"}
+    result = task_collection.insert_one(test_task)
+    return {"message": "task inserted", "id": str(result.inserted_id)}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
