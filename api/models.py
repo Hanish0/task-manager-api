40802f1dffe62082910326dc6e06348e6d0f7d0d
Hanish0 <128824988+Hanish0@users.noreply.github.com>
2025-04-07 23:04:10 +0530

    modify mongodb connection

--- api/models.py
+++ api/models.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
+import os
 
-client  = MongoClient('mongodb://localhost:27017/')
-db = client['taskdb']
+mongo_url = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/')
+client = MongoClient(mongo_url)
 
+db_name = os.environ.get('DB_NAME', 'taskdb')
+db = client[db_name]
 task_collection = db['tasks']
